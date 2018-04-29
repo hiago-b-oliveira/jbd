d4555cf4929086cdfca905fb066e51099db92c35
hiago.oliveira <hiagodeborges+git@gmail.com>
2018-04-29 03:15:53 -0300

    Criação do projeto via start.spring.io;

---
+++ weblogin/src/main/java/io/jbd/weblogin/WebloginApplication.java
@@ -0,0 +1,12 @@
+package io.jbd.weblogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebloginApplication.class, args);
+	}
+}
---
+++ weblogin/src/test/java/io/jbd/weblogin/WebloginApplicationTests.java
@@ -0,0 +1,16 @@
+package io.jbd.weblogin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebloginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
